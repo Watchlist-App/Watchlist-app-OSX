da79443b0c7511fa4beb047ef633f4c22f018078
Dmitry Mazuro <dmitry.mazuro@icloud.com>
2013-04-09 09:37:07 +0300

    Initial Commit

---
+++ WatchList/WLSTAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  WLSTAppDelegate.h
+//  WatchList
+//
+//  Created by Dmitry Mazuro on 09/04/2013.
+//  Copyright (c) 2013 dmitry.mazuro. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface WLSTAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ WatchList/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  WatchList
+//
+//  Created by Dmitry Mazuro on 09/04/2013.
+//  Copyright (c) 2013 dmitry.mazuro. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
