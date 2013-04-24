a174a6ae9dc88a61c031cfeda14c0903d9f5fdcb
Dmitry Mazuro <dmitry.mazuro@icloud.com>
2013-04-24 11:55:31 +0300

    added coreData bindings to window controller

--- WatchList/WatchlistWindowController.m
+++ WatchList/WatchlistWindowController.m
@@ -7,20 +7,23 @@
 //
 
 #import "WatchlistWindowController.h"
+#import "IMDBFetcher.h"
 
 @interface WatchlistWindowController ()
 @property (weak) IBOutlet NSView *managedView;
 @property (weak) IBOutlet NSTableView *sidebarTableView;
-@property (weak) IBOutlet NSTableView *searchResultsTableView;
 @property (strong) IBOutlet NSView *detailedDescriptionView;
 @property (weak) IBOutlet NSCollectionView *postersCollectionView;
-@property (weak) IBOutlet NSTableView *watchlistTableView;
 @property (strong) IBOutlet NSArrayController *searchResultsArrayController;
-@property (strong) IBOutlet NSScrollView *searchResultsScrollView;
 @property (weak) IBOutlet NSTextField *searchTextField;
 @property (strong) IBOutlet NSViewController *popoverViewController;
 @property (strong) IBOutlet NSPopover *popover;
-
+@property (strong) IBOutlet NSScrollView *postersScrollView;
+@property (weak) IBOutlet NSProgressIndicator *searchProgressSpinner;
+@property (weak) IBOutlet NSTableView *searchResultsTable;
+@property (strong) IBOutlet NSScrollView *searchResultsScrollView;
+@property (weak) IBOutlet NSScrollView *watchlistScrollView;
+@property (weak) IBOutlet NSTableView *watchlistTable;
 
 @end
 
@@ -30,9 +33,13 @@
 - (void)windowDidLoad{
     [super windowDidLoad];
     [self.window setBackgroundColor:[NSColor colorWithPatternImage:[NSImage imageNamed:@"23"]]];
-    [self.managedView addSubview:self.searchResultsScrollView];
-    NSDictionary *movie = [NSDictionary dictionaryWithObjectsAndKeys:@"spartacus",@"title",@"2013",@"year",nil];
-    NSArray *searchResults = [NSArray arrayWithObject:movie];
+    //[self.managedView addSubview:self.searchResultsScrollView];
+    NSImage* photo = [NSImage imageNamed:@"band-of-brothers-poster-1.jpg"];
+    NSDictionary *movie = [NSDictionary dictionaryWithObjectsAndKeys:@"spartacus",@"title",@"2013",@"year", photo, @"photo",nil];
+    NSMutableArray *searchResults = [NSMutableArray arrayWithObject:movie];
+    for (int i = 0; i < 100; i++) {
+        [searchResults addObject:movie];
+    }
     self.searchResultsArrayController.content = searchResults;
     
 }
@@ -41,15 +48,38 @@
 }
 
 - (IBAction)searchClicked:(id)sender {
+    [self.searchProgressSpinner startAnimation:self];
+    dispatch_queue_t fetchQueue = dispatch_queue_create("IMDB Fetch", NULL);
+    dispatch_async(fetchQueue, ^{
+        self.searchResultsArrayController.content = [IMDBFetcher searchMoviesByTitle:self.searchTextField.stringValue];
+        [self.searchResultsArrayController.content enumerateObjectsUsingBlock:^(id object, NSUInteger index, BOOL *stop){
+            NSURL *imageURL = [NSURL URLWithString:[object valueForKey:@"poster"]];
+            NSData *imageData = [NSData dataWithContentsOfURL:imageURL];
+            NSImage *posterImage;
+            if (imageData) {
+                posterImage = [[NSImage alloc] initWithData:imageData];
+            } else{
+                posterImage = [NSImage imageNamed:@"NSUser"];
+            }
+            
+            [object setValue:posterImage forKey:@"photo"];
+        }];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.searchProgressSpinner stopAnimation:self];
+        });
+    });
 }
 
 - (IBAction)addMovieClicked:(NSButton*)sender {
     [self.popover showRelativeToRect:sender.bounds ofView:sender preferredEdge:NSMinYEdge];
 }
 
-- (IBAction)switchViewClicked:(id)sender {
-    
-    [self.managedView replaceSubview:self.searchResultsScrollView with:self.postersCollectionView];
+- (IBAction)switchViewClicked:(NSSegmentedControl*)sender {
+    if (sender.selectedSegment == 0) {
+        [self.window.contentView replaceSubview:self.postersScrollView with:self.watchlistScrollView];
+
+    } else
+    [self.window.contentView replaceSubview:self.watchlistScrollView with:self.postersScrollView];
 }
 
 @end
