ba205f1ce011000b9e5f2fe543fed7400573e29a
Dmitry Mazuro <dmitry.mazuro@icloud.com>
2013-10-13 13:50:31 +0300

    Added api key file to gitignore

--- WatchList/TheMovieDbApiKey.h
+++
@@ -1,9 +0,0 @@
-//
-//  TheMovieDbApiKey.h
-//  WatchList
-//
-//  Created by Dmitry Mazuro on 09/05/2013.
-//  Copyright (c) 2013 dmitry.mazuro. All rights reserved.
-//
-
-#define TheMovieDbAPIKey @"86afaae5fbe574d49418485ca1e58803"
